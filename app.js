08fbd51602cfe5b3a8d3bbce8f40a84ee980921c
alvaro004 <alvarollano04@gmail.com>
2021-10-30 05:04:29 -0300

    juego listo

--- app.js
+++ app.js
@@ -11,52 +11,86 @@ const app = Vue.createApp({
 			monsterhealth: 100,
 			specialAttackCount: 0,
 			healCount: 0,
+			gameStatus: null,
+			logMessages: [],
 		};
+	},
+	watch: {
+		//controling the status game
+		//player lost
+		playerHealth(value) {
+			if (value <= 0) this.gameStatus = `Lost`;
+			//draw
+			if (value <= 0 && this.monsterhealth <= 0) this.gameStatus = `Draw`;
+
+			// set the player health equal to 0
+			if (value < 0) this.playerHealth = 0;
+		},
+		//player win
+		monsterhealth(value) {
+			//win
+			if (value <= 0) this.gameStatus = `Win`;
+			//draw
+			if (value <= 0 && this.playerHealth <= 0) this.gameStatus = `Draw`;
+
+			//set the health monster equal to 0
+			if (value < 0) this.monsterhealth = 0;
+		},
 	},
 	methods: {
 		playerAttack() {
-			if (this.playerHealth > 0) {
-				if (this.monsterhealth > 0) {
-					const attackValue = randomDamge(4, 8);
-					this.monsterhealth -= attackValue;
-					this.monsterAttack();
-				}
-			}
-
-			if (this.playerHealth <= 0) {
-				this.playerHealth = 0;
+			if (this.playerHealth > 0 && this.monsterhealth > 0) {
+				const attackValue = randomDamge(4, 8);
+				this.monsterhealth -= attackValue;
+				this.monsterAttack();
+				this.displayMessage('Player', 'attack', attackValue);
 			}
 		},
 		monsterAttack() {
 			if (this.playerHealth > 0) {
 				const attackValue = randomDamge(5, 10);
+				this.displayMessage('Monster', 'counter', attackValue);
 				this.playerHealth -= attackValue;
-			}
-
-			if (this.monsterhealth < 0) {
-				this.monsterhealth = 0;
 			}
 		},
 		specialAttack() {
-			this.specialAttackCount += 1;
 			if (this.playerHealth > 0) {
-				const attackValue = randomDamge(8, 16);
+				this.specialAttackCount += 1;
+				const attackValue = randomDamge(8, 10);
 				if (this.monsterhealth > 0) {
 					this.monsterhealth -= attackValue;
 					this.monsterAttack();
+					this.displayMessage('Player', `special attack`, attackValue);
 				}
 			}
 		},
 		healPlayer() {
-			if (this.playerHealth >= 0 && this.playerHealth < 100) {
+			if (this.playerHealth > 0 && this.playerHealth < 100) {
 				this.healCount += 1;
 				const healthValue = randomDamge(10, 15);
-				console.log(healthValue);
+				this.displayMessage('Player', `heal`, healthValue);
 				this.playerHealth += healthValue;
-				if (this.playerHealth > 100) {
-					this.playerHealth = 100;
-				}
+
+				if (this.playerHealth > 100) this.playerHealth = 100;
 			}
+		},
+		surrenderPlayer() {
+			if (this.monsterhealth > 0) this.playerHealth = 0;
+		},
+		resetGame() {
+			this.gameStatus = null;
+			this.playerHealth = 100;
+			this.monsterhealth = 100;
+			this.specialAttackCount = 0;
+			this.healCount = 0;
+			this.logMessages = [];
+		},
+		displayMessage(who, what, value) {
+			this.logMessages.unshift({
+				byWho: who,
+				whatThing: what,
+				forMany: value,
+			});
 		},
 	},
 	computed: {
@@ -72,26 +106,18 @@ const app = Vue.createApp({
 		changeColorBarMonster() {
 			let classToBeApply = null;
 
-			if (this.monsterhealth < 50) {
-				classToBeApply = 'halfColorMOnsterHealth';
-			}
+			if (this.monsterhealth < 50) classToBeApply = 'halfColorMOnsterHealth';
 
-			if (this.monsterhealth < 15) {
-				classToBeApply = 'dangerColorMonsterHealth';
-			}
+			if (this.monsterhealth < 15) classToBeApply = 'dangerColorMonsterHealth';
 
 			return classToBeApply;
 		},
 		changeColorBarPlayer() {
 			let classToBeApply = null;
 
-			if (this.playerHealth < 50) {
-				classToBeApply = 'halfColorMOnsterHealth';
-			}
+			if (this.playerHealth < 50) classToBeApply = 'halfColorMOnsterHealth';
 
-			if (this.playerHealth < 15) {
-				classToBeApply = 'dangerColorMonsterHealth';
-			}
+			if (this.playerHealth < 15) classToBeApply = 'dangerColorMonsterHealth';
 
 			return classToBeApply;
 		},
@@ -99,7 +125,6 @@ const app = Vue.createApp({
 			return this.specialAttackCount >= 2;
 		},
 		disableBtnHeal() {
-			console.log(this.healCount);
 			return this.healCount >= 3;
 		},
 	},
